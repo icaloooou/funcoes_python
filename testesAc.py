e26feba812101317fbb4d00d6021ecb3ee0a5c65
Ingrid Calou Batista <72050304+icaloooou@users.noreply.github.com>
2021-02-20 03:23:28 -0300

    Update testesAc.py

--- testesAc.py
+++ testesAc.py
@@ -1,6 +1,6 @@
 ##exercicio 1
-#Escreva uma função com o nome pertence, que recebe como argumentos de entrada uma tupla e um item e retorna True,
-#se o item estiver armazenado na tupla, e False, caso contrário.
+#Nesse exercicio deveriamos implementar a função 'pertence', que recebe dois parâmetros como entrada - uma tupla e um item - e que deveria retornar o valor True caso o item
+#estivesse nessa tupla e False, caso não estivesse.
 
 def pertence(tupla, item):
     if item in tupla:
@@ -12,9 +12,9 @@ tupla = (2, 3, 4)
 s = pertence(tupla, 3)
 print(s)
 
-##exercicio 2
-#Escreva uma função chamada substituir que recebe como argumentos de entrada uma lista e dois itens (velho e novo)
-#e retorna uma lista onde todas as ocorrências do item velho são substituídas pelo item novo. 
+##exercicio 2 
+#Nessa função chamada 'substituir', que recebe três parâmetros - uma lista, um item velho e um item novo - era necessário que fosse retornado uma lista onde todas as ocorrências
+#do item velho fossem substituidos pelo item novo.
 
 def substituir(lista, velho, novo):
     while velho in lista:
@@ -23,12 +23,11 @@ def substituir(lista, velho, novo):
     return lista
 
 lista = [1, 2, 2, 4]
-lista = substituir(lista, 1, '45')
+lista = substituir(lista, 2, '45')
 print(lista)
 
 ##exercicio 3
-#Escreva uma função chamada posicoes_lista que recebe como argumentos de entrada uma lista e um item,
-#e retorna uma lista contendo todas os índices em que o item aparece na lista. 
+#A função 'posicoes_lista' deveria receber dois parâmetros - uma lista e um item - para que ela pudesse retornar uma outra lista apenas dos índices em que o item aparece.
 
 def posicoes_lista(lista, item):
     lista2 = lista[:]
@@ -46,8 +45,8 @@ s = posicoes_lista(lista, 1)
 print(s)
 
 #exercicio 4
-#Suponha um dicionário onde a chave é o nome de um aluno e o valor uma lista de notas. Escreva uma função chamada aprovados que recebe como
-#argumentos de entrada o dicionário e retorna uma lista com o nome dos alunos aprovados (um aluno é aprovado quando a média das suas notas é maior ou igual a 6).
+#Na função 'aprovados' o objetivo era receber um dicionário como parâmetro e retornar uma lista com o nome dos alunos que foram aprovados. Lembrando que
+#o dicionário deve conter como chave o nome dos alunos e como valor uma lista de notas. Um aluno só é aprovado com média acima ou igual a 6.
 
 def aprovados(alunos):
     glorificados = []
@@ -58,27 +57,27 @@ def aprovados(alunos):
             glorificados.append(a)
     return glorificados
     
-alunos = {'Ingrid': [4.5, 7.0, 10.0, 10.0], 'Denise': [9.0, 8.5], 'Ana Paula': [3.5, 1.0, 6.5], 'Marcelo': [9.0, 10.0, 7.0, 7.0]}
+alunos = {'Ingrid': [4.5, 7.0, 6.0, 3.0], 'Denise': [9.0, 8.5], 'Ana Paula': [3.5, 1.0, 6.5], 'Marcelo': [9.0, 10.0, 7.0, 7.0]}
 ok = aprovados(alunos)
 print(ok)
 
 #exercicio 5
-#Suponha um dicionário onde a chave é o nome de um aluno e o valor uma lista de notas. Escreva uma função chamada incluir_nota que recebe como argumentos
-#de entrada o dicionário, o nome de um aluno e uma nota. A função deve inserir a nota na lista de notas do aluno correspondente e retornar o dicionário com
-#as alterações realizadas.
+#A função 'incluir_nota' faria com que dentro de um dicionário, contendo como chave nome de alunos e como valor uma lista de notas, fosse possível incluir uma nota
+#à algum aluno desejado. A função recebe três parâmetros: o dicionário, o nome do aluno em que a nota será atribuida e o valor da nota e no final deve retornar o dicionário com
+#as atualização realizadas.
 
 def incluir_nota(alunos, nome, nota):
     if nome in alunos:
         alunos[nome].append(nota)
     return alunos
 
-alunos = {'Ingrid': [4.5, 7.0, 10.0, 10.0], 'Denise': [9.0, 8.5], 'Ana Paula': [3.5, 1.0, 6.5], 'Marcelo': [9.0, 10.0, 7.0, 7.0]}
+alunos = {'Ingrid': [4.5, 7.0, 6.0, 3.0], 'Denise': [9.0, 8.5], 'Ana Paula': [3.5, 1.0, 6.5], 'Marcelo': [9.0, 10.0, 7.0, 7.0]}
 insercao = incluir_nota(alunos, 'Denise', 10.0)
 print(insercao)
 
 #exercicio 6
-#Suponha um dicionário onde a chave é o nome de um aluno e o valor uma lista de notas. Escreva uma função chamada maiores_notas que recebe como argumentos 
-#de entrada o dicionário e retorna outro dicionário com o nome e a maior nota de cada aluno.
+#Nessa função o dicionário será igual as duas questões acima, o nome de um aluno como chave e como valor uma lista de notas. A função terá que receber como parâmetro esse 
+#dicionário e devolver outro dicionário apenas com o nome dos alunos e suas maiores notas.
 
 def maiores_notas(alunos):
     maiores = {}
